78d54e55e4c286d2dda95a5190cd7d6ab10c4f03
Codiving <codivingmoon@gmail.com>
2025-02-13 19:45:45 +0900

    feat: ProgressBar 컴포넌트 구현
    스크롤 위치에 따라 색이 채워지는 ProgressBar 컴포넌트

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useScrollProgress } from "@/hooks";
+
+export default function ProgressBar() {
+  const progress = useScrollProgress();
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-1 bg-gray-200 z-50">
+      <div
+        className={"h-full bg-blue-500 transition-all"}
+        style={{ width: `${progress}%` }}
+      />
+    </div>
+  );
+}
