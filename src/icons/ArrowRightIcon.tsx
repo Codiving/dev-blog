9f6c3c99f70622b9232fc5d309dd280a9eb38a25
Codiving <codivingmoon@gmail.com>
2025-02-13 19:01:21 +0900

    feat: 개발에 필요한 icon 컴포넌트 구현

---
+++ src/icons/ArrowRightIcon.tsx
@@ -0,0 +1,21 @@
+const ArrowRightIcon = (props?: IconProps) => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d={"M5 11.472 8.664 7.8 5 4.128 6.128 3l4.8 4.8-4.8 4.8L5 11.472z"}
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
+
+export default ArrowRightIcon;
