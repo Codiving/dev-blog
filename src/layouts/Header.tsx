38014589b907380e4a388ba832b128560b5aabc0
Codiving <codivingmoon@gmail.com>
2025-02-17 18:45:09 +0900

    chore: 전반적인 css 수정
    배경색 추가
    progressbar 키워드 추가

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,4 +1,4 @@
-import ProgressBar from "@/components/ProgressBar";
+import { ProgressBar } from "@/components";
 
 interface HeaderProps {
   readonly children: React.ReactNode;
@@ -6,7 +6,7 @@ interface HeaderProps {
 
 export default function Header({ children }: HeaderProps) {
   return (
-    <header className="h-header mt-1 w-full sticky top-0 bg-red-500 z-10 flex flex-col justify-center">
+    <header className="h-header mt-progressbar w-full sticky top-0 bg-gray-500 z-10 flex flex-col justify-center">
       <ProgressBar />
       <div>{children}</div>
     </header>
--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -2,7 +2,9 @@ type MainProps = React.HTMLAttributes<HTMLDivElement>;
 
 export default function Main({ children }: MainProps) {
   return (
-    <main className={`flex-1 p-4 transition-all duration-300 ease-in-out`}>
+    <main
+      className={`bg-slate-50 flex-1 p-4 transition-all duration-300 ease-in-out`}
+    >
       {children}
     </main>
   );
