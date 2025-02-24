7f6e3fd01c64d047e91b9276fc468a19a15f524e
Codiving <codivingmoon@gmail.com>
2025-02-24 19:51:49 +0900

    chore: 전반적인 css 수정

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -8,7 +8,7 @@ export default function Sidebar({ isWideView, children }: SidebarProps) {
   return (
     <div
       className={clsx(
-        `transition-all duration-300 ease-in-out overflow-hidden bg-gray-200`,
+        `transition-all duration-300 ease-in-out overflow-hidden bg-gray-200 p-4`,
         isWideView ? "w-0 opacity-0" : "w-sidebar opacity-100"
       )}
     >
