4496d09b6954ee934f8107d65fdc881aafeebfcd
Codiving <codivingmoon@gmail.com>
2025-03-02 16:11:33 +0900

    feat: Post 읽고 있는 목차 bold 처리

--- src/components/PostNavigation.tsx
+++ src/components/PostNavigation.tsx
@@ -1,21 +1,29 @@
 "use client";
 
-import { Heading } from "@/hooks/useContentHeadings";
+import { useClosestHeading } from "@/hooks";
+import useContentHeadings, { Heading } from "@/hooks/useContentHeadings";
 import useScrollToElementById from "@/hooks/useScrollToElementById";
+import clsx from "clsx";
 
-interface PostNavigationProps {
-  headings: Heading[];
-}
-
-export default function PostNavigation({ headings }: PostNavigationProps) {
+export default function PostNavigation() {
+  const headings: Heading[] = useContentHeadings();
   const scrollToElementById = useScrollToElementById();
+  const head = useClosestHeading();
 
   return (
-    <nav className="bg-slate-400 w-56 hidden shrink-0 lg:block">
-      <ul>
-        {headings.map(({ text }, key) => (
-          <li key={key} onClick={() => scrollToElementById(text)}>
-            {text}
+    <nav className="w-56 hidden shrink-0 lg:block">
+      <ul className="sticky top-[100px] flex flex-col gap-[16px]">
+        {headings.map(({ id }, key) => (
+          <li
+            className={clsx("cursor-pointer", {
+              "font-bold": head === id,
+            })}
+            key={key}
+            onClick={() =>
+              scrollToElementById(id.toLocaleLowerCase().replaceAll(" ", "-"))
+            }
+          >
+            {id}
           </li>
         ))}
       </ul>
