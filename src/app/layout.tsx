101e11bc8cbb4a0047e0fe9e04433a1492ff22fe
Codiving <codivingmoon@gmail.com>
2025-04-20 12:44:01 +0900

    feat: 폴더 우선순위 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,47 @@ import { buildFolderStructure } from "@/libs/fetchPosts";
 import type { Metadata } from "next";
 import "./globals.css";
 
+const sortOrder: Record<string, string[]> = {
+  "": ["회고록", "프로그래밍", "에러"],
+  회고록: ["2025"],
+  프로그래밍: [
+    // "HTML",
+    // "CSS",
+    // "JavaScript",
+    // "TypeScript",
+    // "React",
+    "Next",
+    "GraphQL",
+    "Jest",
+  ],
+  에러: ["GitHub"],
+};
+
+function sortFolders(data: Folder[], parentFolderName = ""): Folder[] {
+  const order = sortOrder[parentFolderName] || [];
+
+  const getOrderIndex = (name: string) => {
+    const index = order.indexOf(name);
+    return index === -1 ? Number.MAX_SAFE_INTEGER : index;
+  };
+
+  return data
+    .map((folder) => {
+      if (folder.children && Array.isArray(folder.children)) {
+        const sortedChildren = sortFolders(
+          folder.children as Folder[],
+          folder.folderName
+        );
+        return { ...folder, children: sortedChildren };
+      }
+      return folder;
+    })
+    .sort(
+      (a, b) =>
+        getOrderIndex(a.folderName || "") - getOrderIndex(b.folderName || "")
+    );
+}
+
 export const metadata: Metadata = {
   title: "Codiving's Books",
   description: "Codiving의 개발 블로그",
@@ -15,7 +56,7 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const folders = await buildFolderStructure();
+  const folders = sortFolders(await buildFolderStructure());
 
   return (
     <html lang="ko">
