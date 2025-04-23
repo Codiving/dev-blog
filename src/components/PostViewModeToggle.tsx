0ab61faa52352122811d11211c4e606558aed651
Codiving <codivingmoon@gmail.com>
2025-04-23 19:33:28 +0900

    feat: 좁게보기 상태에서 width 값 줄어들면 wide view로 수정

--- src/components/PostViewModeToggle.tsx
+++ src/components/PostViewModeToggle.tsx
@@ -2,10 +2,24 @@
 
 import { CollapseIcon, ExpandIcon } from "@/icons";
 import { useAppStore } from "@/stores";
+import { useEffect } from "react";
 
 export default function PostViewModeToggle() {
   const isWideView = useAppStore((state) => state.isWideView);
   const onToggleWideView = useAppStore((state) => state.onToggleWideView);
+
+  useEffect(() => {
+    const onClose = () => {
+      if (!isWideView && window.innerWidth < 1270) {
+        onToggleWideView();
+      }
+    };
+    window.addEventListener("resize", onClose);
+
+    return () => {
+      window.removeEventListener("resize", onClose);
+    };
+  }, [isWideView, onToggleWideView]);
 
   return (
     <div className="inline-flex justify-end">
