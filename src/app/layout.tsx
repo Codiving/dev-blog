81096db6e923d689332a7395f359fd065f803bb3
Codiving <codivingmoon@gmail.com>
2025-05-24 17:06:28 +0900

    Codiving 로고 font 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,7 +88,9 @@ export default async function RootLayout({
                 <span className="w-[40px] h-[40px] leading-[40px] text-center text-[24px] bg-black text-white rounded-[12px]">
                   C
                 </span>
-                <span className="text-[18px] font-bold italic">Codiving</span>
+                <span className="robotoMono text-[18px] font-bold italic">
+                  Codiving
+                </span>
               </div>
             </Link>
           </header>
