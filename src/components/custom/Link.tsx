1d9e64c65f575d22b6f535f6c4a2dee306b6bdab
Codiving <codivingmoon@gmail.com>
2025-07-07 19:53:32 +0900

    Link 컴포넌트 css 수정

--- src/components/custom/Link.tsx
+++ src/components/custom/Link.tsx
@@ -1,25 +1,26 @@
 import clsx from "clsx";
 
-const 노링크 = "- 노링크";
+const 레퍼런스 = " - reference";
 
 export default function Link(
-  props: React.AnchorHTMLAttributes<HTMLAnchorElement>
+  props: React.AnchorHTMLAttributes<HTMLAnchorElement>,
 ) {
   const href = props.href || "";
   const children = props.children as string;
   const isExternal = href.startsWith("http");
-  const noLink = children.includes(노링크);
+  const isReference = children.includes(레퍼런스);
 
   return (
     <a
       href={href}
-      className={clsx("font-bold", {
-        "text-green-600": !noLink,
+      className={clsx("transition-colors duration-300", {
+        "hover:text-gray-400": isReference,
+        "text-[#2964aa] hover:text-[#749ac8]": !isReference,
       })}
       target={isExternal ? "_blank" : undefined}
       rel={isExternal ? "noopener noreferrer" : undefined}
     >
-      {children.replace(노링크, "")}
+      {children.replace(레퍼런스, "")}
     </a>
   );
 }
