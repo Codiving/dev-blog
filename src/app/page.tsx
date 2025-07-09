62612c6b666a6dd67da77d49101c5d3e39b8f3eb
Codiving <codivingmoon@gmail.com>
2025-07-09 19:45:04 +0900

    테스트 용 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,55 +29,53 @@ export default async function Home() {
 
   return (
     <ul>
-      {[...results, ...results, ...results, ...results, ...results].map(
-        (result, index) => {
-          const { frontmatter, mainCategory, subCategory, postName } = result;
-          const { title, summary, date, thumbnail } = frontmatter;
-          const postUrl = [mainCategory, subCategory, postName].join("/");
+      {results.map((result, index) => {
+        const { frontmatter, mainCategory, subCategory, postName } = result;
+        const { title, summary, date, thumbnail } = frontmatter;
+        const postUrl = [mainCategory, subCategory, postName].join("/");
 
-          const url = `https://raw.githubusercontent.com/${
-            process.env.USER_NAME
-          }/${process.env.REPOSITORY_NAME}/${
-            process.env.BRANCH_NAME
-          }/${decodeURIComponent(postUrl + "/" + thumbnail)}`;
-          return (
-            <li key={index} className="group">
-              <Link
-                href={"/" + postUrl}
-                className="flex flex-col items-center gap-8 sm:flex-row mb-[20px] pb-[12px]"
-                style={{
-                  borderBottom: "1px solid #e0e0e0",
-                }}
-              >
-                <Image
-                  alt={`${postName} 썸네일`}
-                  src={url}
-                  width={300}
-                  height={150}
-                  className="w-[200px] h-[120px] sm:w-sidebar sm:h-[150px] object-cover shrink-0 transition-transform duration-200 group-hover:scale-105"
-                />
-                <div className="relative flex flex-col justify-between gap-[8px]">
-                  <h3 className="text-[22px] font-bold group-hover:text-[#3ea2ff] transition-colors duration-200">
-                    {title}
-                  </h3>
-                  <p className="line-clamp-4 summary text-[14px]">{summary}</p>
-                  <div
-                    className="flex items-center gap-4 text-[13px]"
-                    style={{
-                      color: "#67717A",
-                    }}
-                  >
-                    <CategoryIcon className="mr-4" />
-                    <span className="text-[13px]">{subCategory}</span>
-                    <span className="text-[13px]">·</span>
-                    <span className="text-[13px]">{date}</span>
-                  </div>
+        const url = `https://raw.githubusercontent.com/${
+          process.env.USER_NAME
+        }/${process.env.REPOSITORY_NAME}/${
+          process.env.BRANCH_NAME
+        }/${decodeURIComponent(postUrl + "/" + thumbnail)}`;
+        return (
+          <li key={index} className="group">
+            <Link
+              href={"/" + postUrl}
+              className="flex flex-col items-center gap-8 sm:flex-row mb-[20px] pb-[12px]"
+              style={{
+                borderBottom: "1px solid #e0e0e0",
+              }}
+            >
+              <Image
+                alt={`${postName} 썸네일`}
+                src={url}
+                width={300}
+                height={150}
+                className="w-[200px] h-[120px] sm:w-sidebar sm:h-[150px] object-cover shrink-0 transition-transform duration-200 group-hover:scale-105"
+              />
+              <div className="relative flex flex-col justify-between gap-[8px]">
+                <h3 className="text-[22px] font-bold group-hover:text-[#3ea2ff] transition-colors duration-200">
+                  {title}
+                </h3>
+                <p className="line-clamp-4 summary text-[14px]">{summary}</p>
+                <div
+                  className="flex items-center gap-4 text-[13px]"
+                  style={{
+                    color: "#67717A",
+                  }}
+                >
+                  <CategoryIcon className="mr-4" />
+                  <span className="text-[13px]">{subCategory}</span>
+                  <span className="text-[13px]">·</span>
+                  <span className="text-[13px]">{date}</span>
                 </div>
-              </Link>
-            </li>
-          );
-        },
-      )}
+              </div>
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
