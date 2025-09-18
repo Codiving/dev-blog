bf349c941fadd35bed15a27bbf3c9b30bd3433b7
Codiving <codivingmoon@gmail.com>
2025-09-18 19:50:44 +0900

    사이트맵 설정 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: "https://www.tech.codiving.kr",
+  siteUrl: "https://codiving.kr",
   generateRobotsTxt: true, // robots.txt 자동 생성
   sitemapSize: 7000, // 너무 많은 URL이면 분할
 };
